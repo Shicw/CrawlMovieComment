01cdeded831958ebe89469f5637220ec773dbf0f
shicw <362559994@qq.com>
2019-10-11 10:32:08 +0800

    remove needless import

--- get_comment.py
+++ get_comment.py
@@ -1,9 +1,6 @@
 import requests
 import json
-import pandas
 import time
-import csv
-import os
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 
