52cf0171d4ef2d58d776626587f6e10426ba507d
shicw <362559994@qq.com>
2019-10-12 16:15:43 +0800

    添加分析功能选择

--- analyse.py
+++ analyse.py
@@ -7,6 +7,7 @@ from pyecharts import options as opts
 
 sex = ['未填写','男','女']#性别对应关系
 font = '../font/msyh.ttf'
+
 #生成词云
 def word_cloud(data, movie_id):
     data = data['content'].tolist()
@@ -27,13 +28,25 @@ def sex_chart(data, movie_id):
     pie.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
     pie.render('user_sex_' + movie_id + '.html')
 
+def choose_func(data, movie_id):
+    print ("[1] 生成词云")
+    print ("[2] 生成性别饼图")
+    func_id = input('请选择您要的分析功能,输入编号:')
+
+    if func_id != '1' and func_id != '2' :
+        print ('请输入正确的功能编号')
+        choose_func(data, movie_id)
+    else:
+        if func_id == '1':
+            word_cloud(data, movie_id)
+        elif func_id == '2':
+            sex_chart(data, movie_id)
+
 def init():
     movie_id = input('请输入电影ID:')
-    #movie_id = '248172'
     if os.path.exists('movie_comment_' + movie_id + '.csv') == True:
         content = pd.read_csv('movie_comment_' + movie_id + '.csv', encoding='utf_8_sig')
-        #word_cloud(content, movie_id)
-        sex_chart(content, movie_id)
+        choose_func(content, movie_id)
     else:
         print ('请先获取该电影的评论数据!')
         init()
